938b5b7a70c7ee1527eb903bfc2c089c0f84e062
rozikhin245 <113223413+rozikhin245@users.noreply.github.com>
2022-09-16 10:27:42 +0700

    kalkulasi suhu rozikhin

---
+++ latihan sabtu.py
@@ -0,0 +1,34 @@
+print("Moh.Khairur Rozikhin")
+print("220441100140")
+print("lab TI")
+
+print("slamat datang di konversi suhu rozikhin :)")
+
+print("jangan di kopas ya arumra gua capek bikin malah di copas")
+print("awas kau")
+
+print("masukkan suhu celcius yang akan di konversikan")
+suhu = int(input())
+suhuu = suhu
+suhuuu = suhu
+suhuuuu = suhu
+suhu = float(suhu * 9) /5 + 32
+print(str(suhuuuu) + " celcius adalah  " + str(suhu) + "  fahrenhait")
+suhu = suhuu + 273
+print(str(suhuuuu) + " celcius adalah  " + str(suhu) + "  kelvin")
+suhu = float(suhuuu * 4)/5
+print(str(suhuuuu) + " celcius adalah  " + str (suhu)+ "  reamur")
+print("....................................")
+print("              selesai               ")
+print("....................................")
+
+
+print("menghitung luas balok")
+print("Masukkan panjang balok" )
+panjang =int(input())
+print("Masukkan lebar balok")
+lebar = int(input())
+print("Masukkan tinggi balok")
+tinggi = int(input())
+luas = panjang*lebar*tinggi
+print("luas balok tersebut adalah  " + str(luas) )
